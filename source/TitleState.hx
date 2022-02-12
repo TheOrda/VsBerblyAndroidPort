d02c29ceba1d3118478bb769fb40b7fe27421b93
TheOrda <91075908+TheOrda@users.noreply.github.com>
2022-02-12 10:38:21 -0300

    agora vai

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -35,7 +35,18 @@ import lime.app.Application;
 import openfl.Assets;
 
 using StringTools;
-
+typedef TitleData =
+{
+	
+	titlex:Float,
+	titley:Float,
+	startx:Float,
+	starty:Float,
+	gfx:Float,
+	gfy:Float,
+	backgroundSprite:String,
+	bpm:Int
+}
 class TitleState extends MusicBeatState
 {
 	public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
